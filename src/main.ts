820f2e49164bd7877d0a39004108186a718700a2
Carolyn-Hope256 <cmhope@ucsc.edu>
2024-11-10 19:41:16 -0800

    Please god work

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const testbutton: HTMLButtonElement = document.createElement("button");
 testbutton.innerHTML = "Click Here!";
 testbutton.onclick = function () {
-    alert("You did it!");
+  alert("You did it!");
 };
 
 app.append(testbutton);
