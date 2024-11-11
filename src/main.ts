76f062d62af4f3e3133766547d74c08efff45b33
Carolyn-Hope256 <cmhope@ucsc.edu>
2024-11-10 19:11:06 -0800

    Actually adds button.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 // todo
-//const app: HTMLDivElement = document.querySelector("#app")!;
+const app: HTMLDivElement = document.querySelector("#app")!;
 
 const testbutton: HTMLButtonElement = document.createElement("button");
 testbutton.innerHTML = "Click Here!";
 testbutton.onclick = function(){
     alert("You did it!");
 }
+app.append(testbutton);
