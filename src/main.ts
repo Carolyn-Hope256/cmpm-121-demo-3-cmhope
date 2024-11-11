945d8584a83766e87e3bd0b096b0cf327ea6b57e
Carolyn-Hope256 <cmhope@ucsc.edu>
2024-11-10 19:14:28 -0800

    Fixes button?

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-// todo
+
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const testbutton: HTMLButtonElement = document.createElement("button");
 testbutton.innerHTML = "Click Here!";
 testbutton.onclick = function(){
     alert("You did it!");
-}
+};
+
 app.append(testbutton);
