c7622d5f27266b76d3e8a0dde4e42afc65a6ea04
Carolyn-Hope256 <cmhope@ucsc.edu>
2024-11-10 19:09:08 -0800

    Adds button.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 // todo
+//const app: HTMLDivElement = document.querySelector("#app")!;
+
+const testbutton: HTMLButtonElement = document.createElement("button");
+testbutton.innerHTML = "Click Here!";
+testbutton.onclick = function(){
+    alert("You did it!");
+}
